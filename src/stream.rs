0b985feffbcace57b8dd0f68dc0ea34eb6811e3d
viri <hi@viri.moe>
2021-03-27 23:18:38 -0600

    stream: add `Backend` enum

--- src/stream.rs
+++ src/stream.rs
@@ -1,6 +1,11 @@
 #[cfg(all(target_os = "windows", feature = "wasapi"))]
 mod wasapi;
 
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub enum Backend {
+    WASAPI,
+}
+
 #[derive(Debug)]
 pub enum Format {
     F32,
